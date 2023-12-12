6dccffb0c1f2f6e74d52992a6ddeb4e8c43f774d
Lucas Rocha <144575286+LucasvoRocha@users.noreply.github.com>
2023-12-12 15:53:28 -0300

    Add files via upload

---
+++ Alunos.java
@@ -0,0 +1,9 @@
+package Membros;
+import java.util.Date;
+
+public class Alunos extends Pessoa {
+  public Alunos (String _nome, String _cpf, Date _data){
+	  super(_nome, _nome, _data); 
+  }
+  public String matricula;
+}
---
+++ Escola.java
@@ -0,0 +1,12 @@
+package Membros;
+import java.util.Scanner;
+import java.util.Date;
+public class Escola {
+
+	public static void main(String[] args) {
+		new Alunos ("nome", "cpf", new Date());
+		new Alunos ("nome", "cpf", new Date());
+		new Alunos ("nome", "cpf", new Date());
+		new Alunos ("nome", "cpf", new Date());
+	}
+}
